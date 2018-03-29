2029a9057c574809b4e07d552a451267188b92f6
yellowb <122517167@qq.com>
2018-03-29 17:33:59 +0800

    Add logger

--- logger/logger.js
+++ logger/logger.js
@@ -1,24 +1,17 @@
-const winston = require('winston');
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, label, printf } = format;
 
-const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.json(),
-    transports: []
+const myFormat = printf(info => {
+    return `${info.timestamp} [${info.label}] ${info.level}: ${info.message}`;
 });
 
-//
-// If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-//
-if (process.env.NODE_ENV !== 'production') {
-    logger.add(new winston.transports.Console({
-        format: winston.format.simple()
-    }));
-} else {
-    logger.add(new winston.transports.File({ filename: 'error.log', level: 'error' }));
-    logger.add(new winston.transports.File({ filename: 'combined.log' }));
-}
-
-logger.info("ssss");
+const logger = createLogger({
+    format: combine(
+        label({ label: 'right meow!' }),
+        timestamp(),
+        myFormat
+    ),
+    transports: [new transports.Console()]
+});
 
 exports.logger = logger;
