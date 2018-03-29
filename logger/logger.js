22a29bd5fc14988413b3bd8e81701cdc09b96be1
yellowb <122517167@qq.com>
2018-03-29 17:28:59 +0800

    Add logger

---
+++ logger/logger.js
@@ -0,0 +1,24 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: []
+});
+
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+        format: winston.format.simple()
+    }));
+} else {
+    logger.add(new winston.transports.File({ filename: 'error.log', level: 'error' }));
+    logger.add(new winston.transports.File({ filename: 'combined.log' }));
+}
+
+logger.info("ssss");
+
+exports.logger = logger;
